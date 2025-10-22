f3dff833113348005ab5c1b49f34c18b1901f037
nomanazhar <nomanaxhar010@gmail.com>
2025-10-22 22:35:48 +0500

    registered the user in database

---
+++ src/api/auth/register/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from "next/server";
+import { dbConnect } from "@/lib/db";
+import  User  from "../../../models/user";
+
+export async function POST(request: NextRequest) {
+  try {
+    await dbConnect();
+    const { name, email, password } = await request.json();
+
+    // Basic validation
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { message: "All fields are required" },
+        { status: 400 }
+      );
+    }
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists in database" },
+        { status: 400 }
+      );
+    }       
+
+    // Create a new user
+    const newUser = await User.create({ name, email, password });
+    if (newUser) {
+      return NextResponse.json(
+        { message: "User registered successfully" },
+        { status: 201 }
+      );
+    }
+  }
+   catch (error) {
+    console.error("Registration error:", error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/lib/db.ts
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/mydatabase";
+
+if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+}
+let cached = global.mongoose;
+
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function dbConnect() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+    if (!cached.promise) {
+       cached.promise = mongoose
+        .connect(MONGODB_URI)
+        .then((mongoose) => mongoose.connection);
+    }
+    try {
+        cached.conn = await cached.promise;
+        return cached.conn;
+    } catch (error) {
+        console.error("Database connection error:", error);
+        throw error;
+    }
+}
